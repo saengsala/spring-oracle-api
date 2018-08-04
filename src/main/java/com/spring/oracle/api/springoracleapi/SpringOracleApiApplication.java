970cbc890948213c4d94dfdce6fca4aa0445463d
Nuttapon Saengsala <nuttapon.sae@pantavanij.com>
2018-08-04 13:27:18 +0700

    initial project Demo project for Spring Boot connect oracle database

---
+++ src/main/java/com/spring/oracle/api/springoracleapi/SpringOracleApiApplication.java
@@ -0,0 +1,12 @@
+package com.spring.oracle.api.springoracleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOracleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOracleApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/spring/oracle/api/springoracleapi/SpringOracleApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.oracle.api.springoracleapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringOracleApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
